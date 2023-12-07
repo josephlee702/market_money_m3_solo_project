bc3ca99ec89565210424d068c636dc11bd52e18c
Joseph Lee <jhjlee702@gmail.com>
2023-12-06 21:22:24 -0700

    finish update action for vendors

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,6 +1,6 @@
 class Api::V0::VendorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
-  rescue_from ActionController::ParameterMissing, with: :wrong_parameters_response
+  # rescue_from ActionController::ParameterMissing, with: :wrong_parameters_response
 
   def show
     render json: VendorSerializer.new(Vendor.find(params[:id]))
@@ -47,8 +47,8 @@ class Api::V0::VendorsController < ApplicationController
       .serialize_json, status: :not_found
   end
 
-  def wrong_parameters(exception)
-    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 422))
-      .serialize_json, status: :unprocessable_entity
-  end
+  # def wrong_parameters(exception)
+  #   render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 422))
+  #     .serialize_json, status: :unprocessable_entity
+  # end
 end
