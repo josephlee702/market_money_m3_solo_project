945de3363b62a06a7a2fbe07e095f3649bc65a09
Joseph Lee <jhjlee702@gmail.com>
2023-12-08 02:00:03 -0700

    find market by state code written - no tests

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -9,6 +9,10 @@ class Api::V0::MarketsController < ApplicationController
     render json: MarketSerializer.new(Market.find(params[:id]))
   end
 
+  def search
+    render json: MarketSerializer.new(Market.find_market_by_state(params)), status: 200
+  end
+
   private
  
   def not_found_response(exception)
--- app/models/market.rb
+++ app/models/market.rb
@@ -11,4 +11,19 @@ class Market < ApplicationRecord
   validates :lat, presence: true
   validates :lon, presence: true
   validates :vendor_count, presence: true
+
+  def self.find_market_by_state(params)
+    # name = params[:name]
+    # city = params[:city]
+    state = params[:state]
+
+    market_info = {}
+
+    # market_info[:name] = name if params[:name] != nil
+    # market_info[:city] = city.capitalize if params[:city] != nil
+    market_info[:state] = state.split.map(&:capitalize).join(" ") if params[:state] != nil
+
+    result = Market.where(market_info)
+    result
+  end
 end
